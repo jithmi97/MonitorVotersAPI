c38a5dd59d4a42677ff5d3b35dc9f01a6b982992
jithmi97 <jithmi.ranatunga@axiatadigitallabs.com>
2023-01-14 19:33:18 +0530

    Final commit

--- src/main/java/com/voters_api_master/voters_monitoring/controller/MonitoringController.java
+++ src/main/java/com/voters_api_master/voters_monitoring/controller/MonitoringController.java
@@ -17,18 +17,18 @@ public class MonitoringController {
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping(value = "monitorVoter/{id}/{url}/{url2}")
-    public String monitorVoter(@PathVariable("id") Long id, @PathVariable("url") String url, @PathVariable("url2") String url2) {
-        String catalog_data = "";
-        String location_data = "";
+    @RequestMapping(value = "monitorVoter/{id}")
+    public String monitorVoter(@PathVariable("id") Long id) {
+        String registeredvoters_data = "";
+        String getVoters_data = "";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>(headers);
-        String catalog_url = String.format("http://%s:8089/getCatalogData/%x", url, id);
-        String location_url = String.format("http://%s:8087/getLocationDataById/%x", url2, id);
-        catalog_data = restTemplate.exchange(catalog_url, HttpMethod.GET, entity, String.class).getBody();
-        location_data = restTemplate.exchange(location_url, HttpMethod.GET, entity, String.class).getBody();
-        return (catalog_data + " " + location_data);
+        String registereddata_url = String.format("http://localhost:8089/getRegisterDataById/%x", id);
+        String getvoters_url = String.format("http://localhost:8088/getVotersData/%x", id);
+        registeredvoters_data = restTemplate.exchange(registereddata_url, HttpMethod.GET, entity, String.class).getBody();
+        getVoters_data = restTemplate.exchange(getvoters_url, HttpMethod.GET, entity, String.class).getBody();
+        return (registeredvoters_data + " " + getVoters_data);
     }
 }
 
